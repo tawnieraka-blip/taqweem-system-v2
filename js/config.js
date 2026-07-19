d2561ba839655f1d531a791d3584113d5cc6ea79
tawnieraka-blip <tawnie.raka@gmail.com>
2026-07-19 10:11:10 +0300

    Add configuration object to config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,17 @@
-// config.js
+/* ==========================================
+   Configuration
+========================================== */
+
+const CONFIG = {
+
+    APP_NAME: "منصة إدارة التقويم والمتابعة",
+
+    VERSION: "1.0.0",
+
+    API_URL: "",
+
+    DEBUG: true
+
+};
+
+Object.freeze(CONFIG);
